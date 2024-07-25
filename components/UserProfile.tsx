6cbfe5413ee941000a353f7ff4509d78ce5fa33c
cyburns <72774499+cyburns@users.noreply.github.com>
2024-07-25 08:06:32 -0400

    styling profile page

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { UserData } from "@/lib/types";
 import { Line, Bar } from "react-chartjs-2";
+import { secondsToHours } from "@/hooks/utils";
 import {
   Chart,
   CategoryScale,
@@ -77,6 +78,16 @@ const UserProfile = ({ userData }: UserProfileProps) => {
     ],
   };
 
+  const averageHeartRate = Math.floor(
+    intervals.reduce(
+      (sum, interval) =>
+        sum +
+        interval.timeseries.heartRate.reduce((sum, data) => sum + data[1], 0) /
+          interval.timeseries.heartRate.length,
+      0
+    ) / intervals.length
+  );
+
   const respiratoryRateData = {
     labels: intervals.flatMap((interval) =>
       interval.timeseries.heartRate.map((data) => formatTime(data[0]))
@@ -95,10 +106,23 @@ const UserProfile = ({ userData }: UserProfileProps) => {
     ],
   };
 
+  const averageRespiratoryRate = Math.floor(
+    intervals.reduce(
+      (sum, interval) =>
+        sum +
+        interval.timeseries.respiratoryRate.reduce(
+          (sum, data) => sum + data[1],
+          0
+        ) /
+          interval.timeseries.respiratoryRate.length,
+      0
+    ) / intervals.length
+  );
+
   const sleepStageData = {
     labels: intervals.flatMap((interval) =>
       interval.stages.map(
-        (stage, index) => `${new Date(interval.ts).toLocaleDateString()}`
+        (_) => `${new Date(interval.ts).toLocaleDateString()}`
       )
     ),
     datasets: [
@@ -107,56 +131,82 @@ const UserProfile = ({ userData }: UserProfileProps) => {
         data: intervals.flatMap((interval) =>
           interval.stages.map((stage) => stage.duration)
         ),
-        backgroundColor: "rgba(255,159,64,0.2)",
-        borderColor: "rgba(255,159,64,1)",
+        backgroundColor: "rgba(6, 128, 247,0.2)",
+        borderColor: "rgba(6, 128, 247,1)",
         borderWidth: 1,
         tension: 0.4,
       },
     ],
   };
 
-  const totalSleepTime = intervals.reduce(
-    (total, interval) =>
-      total +
-      interval.stages.reduce(
-        (stageTotal, stage) => stageTotal + stage.duration,
-        0
-      ),
-    0
-  );
+  const stageDurations = intervals
+    .flatMap((interval) => interval.stages)
+    .reduce((acc, stage) => {
+      acc[stage.stage] = (acc[stage.stage] || 0) + stage.duration;
+      return acc;
+    }, {});
 
   return (
-    <div className="flex justify-center items-center mx-auto flex-col max-w-7xl w-full mt-16">
-      <div className="mb-10 flex flex-row justify-between w-full items-center px-10">
-        <div>
-          <h1 className="text-[5rem] font-medium!leading-[1]">{user.name}</h1>
-          <p className="text-lg font-thin">
-            Total Sleep Time: {totalSleepTime} seconds
-          </p>
+    <div className="flex justify-center items-center mx-auto flex-col w-full mt-16 mb-32 p-5 max-w-fit">
+      <div className="mb-10 flex flex-row items-center px-10">
+        <h1 className="text-[5rem] font-medium!leading-[1]">{user.name}</h1>
+      </div>
+
+      <div className="flex flex-col lg:flex-row">
+        <div className="w-[40rem] bg-[#252526] p-4 rounded-lg flex justify-center m-2 bg-opacity-60">
+          <Line data={heartRateData} options={chartOptions} />
         </div>
-        <div>
-          <h2 className="text-[6rem] text-center font-medium!leading-[1]">
-            {intervals[0]?.score}
+
+        <div className="bg-[#252526] py-7 lg:py-3 px-10 rounded-lg flex flex-col justify-center m-2 bg-opacity-60">
+          <h2 className="text-[6rem] text-center font-medium !leading-[1]">
+            {averageHeartRate}
           </h2>
-          <p className="text-lg text-center font-thin">Average sleep score</p>
+          <p className="text-lg text-center font-thin">Average heart rate</p>
         </div>
       </div>
 
       <div className="flex flex-col lg:flex-row">
-        <div className="w-[40rem] bg-[#252526] p-3 rounded-lg flex justify-center m-2 bg-opacity-60">
-          <Line data={heartRateData} options={chartOptions} />
+        <div className="bg-[#252526] py-7 lg:py-3 px-[3.2rem] rounded-lg flex flex-col justify-center m-2 bg-opacity-60">
+          <h2 className="text-[6rem] text-center font-medium !leading-[1]">
+            {averageRespiratoryRate}
+          </h2>
+          <p className="text-lg text-center font-thin">
+            Average
+            <br /> respiratory rate
+          </p>
         </div>
 
         <div className="w-[40rem] bg-[#252526] p-3 rounded-lg flex justify-center m-2 bg-opacity-60">
           <Line data={respiratoryRateData} options={chartOptions} />
         </div>
       </div>
+
       <div className="flex flex-col lg:flex-row">
         <div className="w-[40rem] bg-[#252526] p-3 rounded-lg flex justify-center m-2 bg-opacity-60">
           <Bar data={sleepStageData} options={chartOptions} />
         </div>
-        <div className="w-[40rem] bg-[#252526] p-3 rounded-lg flex justify-center m-2 bg-opacity-60">
-          <Bar data={sleepStageData} options={chartOptions} />
+
+        <div className="bg-[#252526] py-7 lg:py-3 px-10 rounded-lg flex flex-col justify-center m-2 bg-opacity-60 space-y-5">
+          <div>
+            <p className="text-center text-4xl">
+              {secondsToHours(stageDurations.awake) || 0}
+            </p>
+            <p className="text-center font-thin">Spent awake</p>
+          </div>
+
+          <div>
+            <p className="text-center text-4xl">
+              {secondsToHours(stageDurations.light) || 0}
+            </p>
+            <p className="text-center font-thin">Spent in light sleep</p>
+          </div>
+
+          <div>
+            <p className="text-center text-4xl">
+              {secondsToHours(stageDurations.deep) || 0}
+            </p>
+            <p className="text-center font-thin">Spent in deep sleep</p>
+          </div>
         </div>
       </div>
     </div>
@@ -164,60 +214,3 @@ const UserProfile = ({ userData }: UserProfileProps) => {
 };
 
 export default UserProfile;
-
-//   const sleepStageData = {
-//     labels: intervals.flatMap((interval) =>
-//       interval.stages.map(
-//         (stage, index) =>
-//           `${new Date(interval.ts).toLocaleDateString()} Stage ${index + 1}`
-//       )
-//     ),
-//     datasets: [
-//       {
-//         label: "Sleep Stages",
-//         data: intervals.flatMap((interval) =>
-//           interval.stages.map((stage) => stage.duration)
-//         ),
-//         backgroundColor: "rgba(255,159,64,0.2)",
-//         borderColor: "rgba(255,159,64,1)",
-//         borderWidth: 1,
-//         tension: 0.4,
-//       },
-//     ],
-//   };
-
-//   const totalSleepTime = intervals.reduce(
-//     (total, interval) =>
-//       total +
-//       interval.stages.reduce(
-//         (stageTotal, stage) => stageTotal + stage.duration,
-//         0
-//       ),
-//     0
-//   );
-//   const stageDurations = intervals
-//     .flatMap((interval) => interval.stages)
-//     .reduce((acc, stage) => {
-//       acc[stage.stage] = (acc[stage.stage] || 0) + stage.duration;
-//       return acc;
-//     }, {});
-
-{
-  /* <h2>Summary</h2>
-      <p>Total Sleep Time: {totalSleepTime} seconds</p>
-      <p>Time in Awake: {stageDurations.awake || 0} seconds</p>
-      <p>Time in Light Sleep: {stageDurations.light || 0} seconds</p>
-      <p>Time in Deep Sleep: {stageDurations.deep || 0} seconds</p>
-      <p>Score: {intervals[0]?.score || "N/A"}</p> */
-}
-
-{
-  /* <div>
-        <h3>Respiratory Rate Over Time</h3>
-        <Line data={respiratoryRateData} />
-      </div>
-      <div>
-        <h3>Sleep Stages</h3>
-        <Bar data={sleepStageData} />
-      </div> */
-}
--- hooks/utils/index.ts
+++ hooks/utils/index.ts
@@ -3,3 +3,10 @@ export const formatDuration = (duration: number) => {
   const minutes = Math.floor((duration % 3600) / 60);
   return `${hours} hours and ${minutes} minutes`;
 };
+
+export const secondsToHours = (seconds: number): string => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+
+  return `${hours}h ${minutes}m`;
+};
